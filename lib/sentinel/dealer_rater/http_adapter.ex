25f81f3367fba28750acea8cfb5d46489e26e556
Samuel Levy <samulevy@gmail.com>
2021-03-30 01:24:51 -0300

    Randomizes the http adapters user agent

--- lib/sentinel/dealer_rater/http_adapter.ex
+++ lib/sentinel/dealer_rater/http_adapter.ex
@@ -25,9 +25,24 @@ defmodule Sentinel.DealerRater.HTTPAdapter do
 
   defp client do
     middlewares = [
-      {Tesla.Middleware.BaseUrl, "https://www.dealerrater.com"}
+      {Tesla.Middleware.BaseUrl, "https://www.dealerrater.com"},
+      {Tesla.Middleware.Headers, [{"user-agent", random_user_agent()}]}
     ]
 
     Tesla.client(middlewares)
+  end
+
+  defp random_user_agent() do
+    [
+      "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/webkit-version (KHTML, like Gecko) Silk/browser-version like Chrome/chrome-version Safari/webkit-version",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Silk/44.1.54 like Chrome/44.0.2403.63 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+      "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9",
+      "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+    ]
+    |> Enum.random()
   end
 end
